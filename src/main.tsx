18f5194a54756273c6d2e170cab8fb302d67de85
achmad anshori <utd.rik@gmail.com>
2023-01-15 01:08:41 +0700

    fix layout, scrollbar, and lazy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './Router';
+
 import './styles/index.css';
+
+import Container from './Layout/Container';
+import Router from './Router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router />
+    <Container>
+      <Router />
+    </Container>
   </React.StrictMode>
 );
