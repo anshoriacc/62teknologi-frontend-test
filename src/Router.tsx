d663e7f571ca918cb8b1fd17777e315ef259c3df
achmad anshori <utd.rik@gmail.com>
2023-01-16 01:23:18 +0700

    fix routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,8 +7,6 @@ import { store } from './redux/store';
 import Layout from './Layout/Layout';
 
 const Home = lazy(() => import('./pages/Home'));
-const Edit = lazy(() => import('./pages/Edit'));
-const Add = lazy(() => import('./pages/Add'));
 const Detail = lazy(() => import('./pages/Detail'));
 
 function Router() {
@@ -19,8 +17,6 @@ function Router() {
           <Suspense fallback={null}>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/add" element={<Add />} />
-              <Route path="/edit/:id" element={<Edit />} />
               <Route path="/:id" element={<Detail />} />
             </Routes>
           </Suspense>
